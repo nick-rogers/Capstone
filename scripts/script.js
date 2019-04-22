381fde8ad48f3a50c12bd14d07d886ab773117bb
nick-rogers <31479865+nick-rogers@users.noreply.github.com>
2019-04-22 01:06:01 -0500

    Mobile Drag

--- scripts/script.js
+++ scripts/script.js
@@ -1,77 +1,154 @@
 $(document).ready(function(){
 
-// Object content 
 
 
 
-// Invert Scroll elements
-$.jInvertScroll(['#main', '#back']);
 
-
-// Function that takes scrolls and activates division events
-var windowScrollCount, count, year, topDist;
-
-$.jInvertScroll(['#main'], {
-	width: 'auto',	// Page width (auto or int value)
-	height: 200000,	// Page height (the shorter, the faster the scroll)
-	onScroll: function(percent) {
-
-		$('#start-scrolling').css('display', 'none');
-
-		year = Math.round(percent*4519);
-
-		if(year == 4519){
-			console.log('complete');
-			$('.year').css({
-				'font-size': '150px',
-				'text-align': 'left',
-				'margin-top': '70px'
-			});
-
-			$('#ending').css('width', '50vw');
-			$('#main').css('margin-left', '-5vw');
-
-		}else {
-			$('.year').css({
-				'font-size': '300px',
-				'text-align': 'center',
-				'margin-top': '0px'
-			});
-
-			$('#ending').css('width', '0vw');
-			$('#main').css('margin-left', '-0vw');
-		}
-
-    	updateYear(year);
-
-    	//console.log('percentage: ' + Math.round(percent*600));
-		$('#cover').css('width', Math.round(600-(percent*600))+'px');
-		// Callback function that will be called each time the user
-		// scrolls up or down, useful for animating other parts
-		// on the page depending on how far the user has scrolled down
-		// values go from 0.0 to 1.0 (with 4 decimals precision)
+/*
+var open = false;
+$('.prog').click(function(){
+	if(open){
+		$('.prog').css('width', '30px');
+		open = false;
+	}else {
+		$('.prog').css('width', '150px');
+		open = true;
 	}
+});
+*/
+
+
+
+
+
+
+
+// Scroll function 
+var scrolled = 0, percentage;
+$(document).scroll(function() {
+    scrolled = $(document).scrollTop();
+    $('.p-bar').css('height', Math.round($(document).scrollTop()/27)+'px');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var martop, marginDiff;
+$('.mobile-section').click(function(){
+
+	// Turns off the ability to scroll and hides nav
+	toggleOverflow();
+	//hideShowNav();
+
+	$('.tri-con').css('display', 'block');
+
+
+	// Collects the margin top distance for object image placement
+	martop = $(this).css('margin-top');
+	martop = parseInt(martop);
+	marginDiff = martop-scrolled;
+
+	// Mobile image is made fixed and placed correctly relative to the division
+	$(".mob-image").css({
+		"position": "fixed", 
+		"top": "0",
+		"top": marginDiff+'px'});
+	
+	// Makes division marks animate, separate 
+	$('.split').css('height', '100vh');
+	$('.split1').css('margin-top', '-100vh');
+	$('.split2').css('margin-top', '0vh');
+	$('.fill').css('height', '100%');
+	$('.fill1').css('bottom', '0');
+	$('.fill2').css('top', '0');
+
+	setTimeout(function(){
+		$('.mi').css('clip-path', 'none');
+		$('.mi').css('webkit-clip-path', 'none');
+	}, 175);
+
+
 });
 
 
 
 
 
-//------------------------------YEAR UPDATES---------------------------------------//
 
 
-// Function to update the year in the background 
-function updateYear(yr){
-	if(yr < 2500){
-	   		yr = 2500-yr;
-	   		yr = yr + " BCE";
-	   	}else if( yr => 2500 && yr <= 4519){
-	   		yr = yr-2500;
-	   		yr = yr + " CE";
-	   	}else {
-	   		yr = 2019 + " CE";
-	   	}
-	$('.year').text(yr);
+// Function to shrink and return division to its default status 
+$('#h-b').click(function(){
+
+	// Toggle the navigation elements and enable scroll
+	toggleOverflow();
+	//hideShowNav();
+
+	$('.tri-con').css('display', 'none');
+
+	setTimeout(function(){
+		$('.mi').css('clip-path', 'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)');
+		$('.mi').css('webkit-clip-path', 'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)');
+	}, 300);
+
+
+	$('.split').css('height', '175px');
+	$('.split1').css('margin-top', '-175px');
+	$('.split2').css('margin-top', '75px');
+	$('.fill').css('height', '0%');
+
+
+	$(".mob-image").css({
+		"position": "absolute", 
+		"top": "0",
+		"top": '0px'});
+
+});
+
+
+
+
+
+var objectOpen = false;
+
+function toggleOverflow (){
+
+	if(objectOpen){
+		$('body').css('overflow-y', 'scroll');
+		objectOpen = false;
+	}else {
+		$('body').css('overflow-y', 'hidden');
+		objectOpen = true;
+	}
+
+};
+
+
+
+var navVis = true;
+function hideShowNav (){
+	if(navVis) {
+		$('.nav-wrap').css('display', 'none');
+		navVis = false;
+	} else {
+		$('.nav-wrap').css('display', 'block');
+		navVis = true;
+	}
+
 }
 
 
@@ -79,24 +156,75 @@ function updateYear(yr){
 
 
 
-// Disable and enable scroll functions
-function disableScroll(){
-	$('body').css('overflow', 'hidden');
-	$('html').css('overflow', 'hidden');
 
+
+/*
+$('.prog').bind("taphold", tapHoldHandler);
+
+function tapHoldHandler(event){
+	$(event.target).css('width', '150px');
+	$(event.target).css('background-color', 'green');
+	console.log('tapheld');
+}	*/
+
+
+
+
+/*
+$('.prog').on("taphold", function(event){
+
+});
+*/
+
+
+/*
+$('.prog').mousedown(function(){
+
+console.log('mouse down');
+
+	$('.prog').css('width', '175px');
+
+});*/
+
+
+function clickHold(){
+	$('.prog').css('width', '175px');
 }
 
-function enableScroll(){
-	$('body').css('overflow', 'auto');
-	$('html').css('overflow', 'auto');
-}
+
+
+
+
+$('.prog').mousedown(function(){
+
+	console.log('down');
+	$('.prog').css('width', '175px');
+	$('.prog').css('background-color', 'green');
+
+}).mouseup(function(){
+
+	$('.prog').css('width', '50px');
+	console.log('up');
+
+});
 
 
 
 
 
+//$('#dt').draggable();
 
 
+
+/*
+var timeoutId = 0;
+
+$('#progID').on('mousedown', function() {
+    timeoutId = setTimeout(clickHold, 1000);
+}).on('mouseup mouseleave', function() {
+    clearTimeout(timeoutId);
+});
+*/
 
 
 
